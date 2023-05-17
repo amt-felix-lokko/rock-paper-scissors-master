b55783fa223e97f5cd09bcdc6e79b64c34064d75
felix-lokko <felix.lokko@amalitech.org>
2023-05-17 20:02:34 +0000

    update

---
+++ app.js
@@ -0,0 +1,9 @@
+const main = document.querySelector('.main');
+const paper = document.querySelector('.paperDiv');
+const scissors = document.querySelector('.scissorsDiv');
+const rock = document.querySelector('.rockDiv');
+const rules = document.querySelector('.rules');
+const rulesPic = document.querySelector('.rulesPic');
+const close = document.querySelector('.close');
+
+
